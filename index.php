189032a5855ff66e0ebf2402aa37bea57c7b0e3d
steven <steve@DESKTOP-7GRC3LA.localdomain>
2020-04-29 22:27:09 +1000

    added files

--- index.php
+++ index.php
@@ -1,2 +1,25 @@
 <?php
-    echo 'hello this is stevens pet book website!!!';
+echo 'Welcome to PetBook';
+?>
+<div id="fb-root"></div>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId            : '529847264357591',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v6.0'
+    });
+  };
+</script>
+<script async defer src="https://connect.facebook.net/en_US/sdk.js"></script>
+<script>
+
+
+
+  FB.api('/me', function(response) {
+                alert("Name: "+ response.name);
+            });
+
+</script>
+
