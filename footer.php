1a362f2cafe15e6c80de3c46b1404b2c5f44f3ee
Kim Cao <s3668190@student.rmit.edu.au>
2020-05-11 02:00:27 +1000

    Edit text in footer

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
 	<!-- Footer -->
 	<footer style="margin-top:30px" class="w3-container w3-theme-d3 w3-padding-16">
-		<h5>Footer</h5>
+		<h5>© Copyright 2020</h5>
 	</footer>
 	<footer class="w3-container w3-theme-d5">
 		<p>
-			Powered and design from <a
+			All rights reserved. Powered and styled by <a
 				href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a>
 		</p>
 	</footer>
